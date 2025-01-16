c72d57d9b007f58ea16a3cba78c07fb4a45d93dc
samie-n-19 <snaukudkar@wpi.edu>
2025-01-16 10:15:14 -0500

    resnext network

--- Phase2/Code/Network/Network.py
+++ Phase2/Code/Network/Network.py
@@ -18,6 +18,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from torchsummary import summary
+import math
 
 def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
@@ -155,9 +157,6 @@ class ResNet(ImageClassificationBase):
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
-
 class ResNeXtBottleneck(nn.Module):
     expansion = 4
 
@@ -186,103 +185,54 @@ class ResNeXtBottleneck(nn.Module):
         out = F.relu(out)
         return out
 
-class ResNeXtBottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_channels, out_channels, stride=1, cardinality=32, base_width=4):
-        super(ResNeXtBottleneck, self).__init__()
-        D = cardinality * base_width
-        self.conv1 = nn.Conv2d(in_channels, D, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(D)
-        self.conv2 = nn.Conv2d(D, D, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=False)
-        self.bn2 = nn.BatchNorm2d(D)
-        self.conv3 = nn.Conv2d(D, out_channels * self.expansion, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels * self.expansion:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels * self.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels * self.expansion)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-class ResNeXt(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10, cardinality=32, base_width=4):
+class ResNeXt(ImageClassificationBase):
+    def __init__(self, num_blocks, cardinality, bottleneck_width, num_classes=10):
         super(ResNeXt, self).__init__()
-        self.in_channels = 64
+        self.cardinality = cardinality
+        self.bottleneck_width = bottleneck_width
+        self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, cardinality=cardinality, base_width=base_width)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, cardinality=cardinality, base_width=base_width)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, cardinality=cardinality, base_width=base_width)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, cardinality=cardinality, base_width=base_width)
-        self.linear = nn.Linear(512 * block.expansion, num_classes)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-    def _make_layer(self, block, out_channels, num_blocks, stride, cardinality, base_width):
+        self.layer1 = self._make_layer(num_blocks[0], 64, stride=1)
+        self.layer2 = self._make_layer(num_blocks[1], 128, stride=2)
+        self.layer3 = self._make_layer(num_blocks[2], 256, stride=2)
+        self.layer4 = self._make_layer(num_blocks[3], 512, stride=2)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.linear = nn.Linear(512 * ResNeXtBottleneck.expansion, num_classes)
+
+    def _make_layer(self, num_blocks, out_channels, stride):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride, cardinality, base_width))
-            self.in_channels = out_channels * block.expansion
+            layers.append(ResNeXtBottleneck(self.in_planes, out_channels, stride, self.cardinality, self.bottleneck_width))
+            self.in_planes = out_channels * ResNeXtBottleneck.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
+        out = self.pool1(out)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
+        out = self.avg_pool(out)
+        out = torch.flatten(out, 1)
         out = self.linear(out)
         return out
 
-    def training_step(self, batch):
-        images, labels = batch
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels) # Calculate loss
-        return loss
-
-    def validation_step(self, batch):
-        images, labels = batch
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels) # Calculate loss
-        acc = accuracy(out, labels)         # Calculate accuracy
-        return {'val_loss': loss.detach(), 'val_acc': acc}
-
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x['val_loss'] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
-        batch_accs = [x['val_acc'] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
-        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
-
-    def epoch_end(self, epoch, result):
-        print(f"Epoch [{epoch}], val_loss: {result['val_loss']:.4f}, val_acc: {result['val_acc']:.4f}")
-
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-
 def ResNeXt50():
-    return ResNeXt(ResNeXtBottleneck, [3, 4, 6, 3], cardinality=32, base_width=4)
+    return ResNeXt(num_blocks=[3, 4, 6, 3], cardinality=32, bottleneck_width=4)
 
-class DenseLayer(nn.Module):
-    def __init__(self, in_channels, growth_rate):
-        super(DenseLayer, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_channels)
-        self.conv1 = nn.Conv2d(in_channels, growth_rate * 4, kernel_size=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(growth_rate * 4)
-        self.conv2 = nn.Conv2d(growth_rate * 4, growth_rate, kernel_size=3, padding=1, bias=False)
+class Bottleneck(ImageClassificationBase):
+    def __init__(self, in_planes, growth_rate):
+        super(Bottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, 4*growth_rate, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(4*growth_rate)
+        self.conv2 = nn.Conv2d(4*growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
 
     def forward(self, x):
         out = self.conv1(F.relu(self.bn1(x)))
@@ -290,86 +240,71 @@ class DenseLayer(nn.Module):
         out = torch.cat([out, x], 1)
         return out
 
-class DenseBlock(nn.Module):
-    def __init__(self, num_layers, in_channels, growth_rate):
-        super(DenseBlock, self).__init__()
-        layers = []
-        for i in range(num_layers):
-            layers.append(DenseLayer(in_channels + i * growth_rate, growth_rate))
-        self.dense_block = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.dense_block(x)
-
-class TransitionLayer(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(TransitionLayer, self).__init__()
-        self.bn = nn.BatchNorm2d(in_channels)
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
-        self.pool = nn.AvgPool2d(2, stride=2)
+class Transition(ImageClassificationBase):
+    def __init__(self, in_planes, out_planes):
+        super(Transition, self).__init__()
+        self.bn = nn.BatchNorm2d(in_planes)
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
 
     def forward(self, x):
         out = self.conv(F.relu(self.bn(x)))
-        out = self.pool(out)
+        out = F.avg_pool2d(out, 2)
         return out
 
-class DenseNet(nn.Module):
-    def __init__(self, num_blocks, num_layers_per_block, growth_rate=12, reduction=0.5, num_classes=10):
+class DenseNet(ImageClassificationBase):
+    def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_classes=10):
         super(DenseNet, self).__init__()
         self.growth_rate = growth_rate
-        num_channels = 2 * growth_rate
 
-        self.conv1 = nn.Conv2d(3, num_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(num_channels)
+        num_planes = 2*growth_rate
+        self.conv1 = nn.Conv2d(3, num_planes, kernel_size=3, padding=1, bias=False)
 
-        blocks = []
-        for i in range(num_blocks):
-            blocks.append(DenseBlock(num_layers_per_block, num_channels, growth_rate))
-            num_channels += num_layers_per_block * growth_rate
-            if i != num_blocks - 1:
-                out_channels = int(num_channels * reduction)
-                blocks.append(TransitionLayer(num_channels, out_channels))
-                num_channels = out_channels
+        self.dense1 = self._make_dense_layers(block, num_planes, nblocks[0])
+        num_planes += nblocks[0]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans1 = Transition(num_planes, out_planes)
+        num_planes = out_planes
 
-        self.blocks = nn.Sequential(*blocks)
-        self.bn2 = nn.BatchNorm2d(num_channels)
-        self.linear = nn.Linear(num_channels, num_classes)
+        self.dense2 = self._make_dense_layers(block, num_planes, nblocks[1])
+        num_planes += nblocks[1]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans2 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense3 = self._make_dense_layers(block, num_planes, nblocks[2])
+        num_planes += nblocks[2]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans3 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense4 = self._make_dense_layers(block, num_planes, nblocks[3])
+        num_planes += nblocks[3]*growth_rate
+
+        self.bn = nn.BatchNorm2d(num_planes)
+        self.linear = nn.Linear(num_planes, num_classes)
+
+    def _make_dense_layers(self, block, in_planes, nblock):
+        layers = []
+        for i in range(nblock):
+            layers.append(block(in_planes, self.growth_rate))
+            in_planes += self.growth_rate
+        return nn.Sequential(*layers)
 
     def forward(self, x):
         out = self.conv1(x)
-        out = self.blocks(out)
-        out = F.relu(self.bn2(out))
+        out = self.trans1(self.dense1(out))
+        out = self.trans2(self.dense2(out))
+        out = self.trans3(self.dense3(out))
+        out = self.dense4(out)
+        out = F.relu(self.bn(out))
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
-    def training_step(self, batch):
-        images, labels = batch
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels) # Calculate loss
-        return loss
-
-    def validation_step(self, batch):
-        images, labels = batch
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels) # Calculate loss
-        acc = accuracy(out, labels)         # Calculate accuracy
-        return {'val_loss': loss.detach(), 'val_acc': acc}
-
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x['val_loss'] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
-        batch_accs = [x['val_acc'] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
-        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
-
-    def epoch_end(self, epoch, result):
-        print(f"Epoch [{epoch}], val_loss: {result['val_loss']:.4f}, val_acc: {result['val_acc']:.4f}")
-
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-
 def DenseNet121():
-    return DenseNet(num_blocks=4, num_layers_per_block=32, growth_rate=12, reduction=0.5, num_classes=10)
+    return DenseNet(Bottleneck, [6,12,24,16], growth_rate=12)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = DenseNet(Bottleneck, [6,12,24,16], growth_rate=12).to(device)
+summary(model, (3, 32, 32))
